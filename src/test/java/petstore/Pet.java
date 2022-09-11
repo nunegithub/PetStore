4e0a125e29904345664bd06883f1add83dfff8e1
nunegithub <tvcroacia@gmail.com>
2022-09-11 00:12:13 -0300

    Finalizado o script excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -56,9 +56,43 @@ public class Pet {
                 .body("tags.id", contains(2021))
                 .body("category.name", is("dog"))
                 .body("category.id", is(1))
-
         ;
 
     }
+        @Test(priority = 3)
+        public void alterarPet() throws IOException {
+            String jsonBody = lerJson("db/pet2.json");
+
+            given()
+                    .contentType("application/json")
+                    .log().all()
+                    .body(jsonBody)
+            .when()
+                    .put(uri)
+            .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("name", is("Nina"))
+                    .body("status", is("sold"))
+            ;
+        }
+
+      @Test(priority = 4)
+        public void excluirPet(){
+        String petId = "777";
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                .when()
+                        .delete(uri + "/" + petId)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("code", is(200))
+                        .body("type",is("unknown"))
+                        .body("message", is(petId))
+                ;
+        }
+
 }
 
